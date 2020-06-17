1db38e4b97bd0a64b62eeb6781fbbbdbe6e87f7f
GabrielVk <50385062+GabrielVk@users.noreply.github.com>
2020-06-17 15:21:47 -0300

    1.55

---
+++ equation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+
+std::string equation()
+{
+    long double value1;
+    long double value2;
+    std::cout << "Enter the first value for the addition\n";
+    std::cin >> value1;
+    std::cout << "Enter the second value for the addition\n";
+    std::cin >> value2;
+    long double result{value1 + value2};
+    std::string result_str= std::to_string(result);
+    return "The result is " + result_str;
+}
--- subtraction.cpp
+++ subtraction.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 std::string subtraction()
 {
     long double value1;
